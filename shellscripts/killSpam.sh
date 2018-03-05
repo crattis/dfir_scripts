493ef6345ea03ee725655998403aa804f2444e77
Chris J <crattis@users.noreply.github.com>
2018-03-05 12:12:50 -0500

    For loop bug fix. Problem with last directory

--- shellscripts/killSpam.sh
+++ shellscripts/killSpam.sh
@@ -7,50 +7,71 @@
 # Chris J
 # [@]rattis
 #
-# version 1.001
+# version 1.002
 ###############################################################################
 
 echo ''
-workDir=$(basename `pwd`)
-for quarFile in $(ls -1);
+for workDir in $(ls -1)
 do
-        # list number of remaining emails
-        echo "---"
-        ls -1 | wc -l
-        echo "---"
+        cd $workDir
+        echo "----"
+        echo $workDir
+        echo "----"
+        #workDir=$(basename `pwd`)
+        for quarFile in $(ls -1);
+        do
+                # list number of remaining emails
+                echo "---"
+                ls -1 | wc -l
+                echo "---"
 
-        ## start email block
-        echo "$quarFile:"
-        # is it zipped or unzipped, and display
-        testFile=$(file --mime-type -b $quarFile)
-        if [ "$testFile" = 'application/gzip' ];
-        then
-                zgrep -E '^(To|From|Subject|Date)\: ' $quarFile
-                echo ''
-        else
-                egrep '^(To|From|Subject|Date)\: ' $quarFile
-                echo ''
-        fi
+                ## start email block
+                echo "$quarFile:"
+                # is it zipped or unzipped, and display
+                testFile=$(file --mime-type -b $quarFile)
+                if [ "$testFile" = 'application/gzip' ];
+                then
+                        zgrep -E '^To\: ' $quarFile
+                        zgrep -E '^Date\: ' $quarFile
+                        zgrep -E '^From\: ' $quarFile
+                        zgrep -E '^Subject\: ' $quarFile
+                        echo ''
+                else
+                        egrep '^To\: ' $quarFile
+                        egrep '^Date\: ' $quarFile
+                        egrep '^From\: ' $quarFile
+                        egrep '^Subject\: ' $quarFile
+                        echo ''
+                fi
 
-        # relase to mailbox or delete file
-        read -p "[*] Do you want to Release or Delete the file? (r/d): " RorD
-        cRorD=$(echo -n $RorD | tr A-Z a-z)
-        if [ X"$cRorD" = X"r" ];
-        # release the file to the recipient
-        then
-                releaseFile="$workDir/$quarFile"
-                amavisd-release $releaseFile
-                echo "[+] email released to user and removed from quarantine "
-                rm $quarFile
-        elif [ X"$cRorD" = X"d" ];
-        # delete the file
-        then
-                rm $quarFile
-                echo "[+] email deleted "
-        else
-        # do nothing and go to next file.
-        echo '[-] nothing done to email, moving to next one'
-        fi
-        echo ''
+                # relase to mailbox or delete file
+                read -p "[*] Do you want to Release the email, Delete the email, or Quit?? (r/d/q): " RorD
+                cRorD=$(echo -n $RorD | tr A-Z a-z)
+                if [ X"$cRorD" = X"r" ];
+                # release the file to the recipient
+                then
+                        releaseFile="$workDir/$quarFile"
+                        amavisd-release $releaseFile
+                        echo "[+] email released to user and removed from quarantine "
+                        rm $quarFile
+                elif [ X"$cRorD" = X"d" ];
+                # delete the file
+                then
+                        rm $quarFile
+                        echo "[+] email deleted "
+                elif [ X"$cRorD" = X"q" ] ;
+                then
+                        echo "[-] User chose to quit"
+                        echo ''
+                        exit 0
+                else
+                # do nothing and go to next file.
+                echo '[-] nothing done to email, moving to next one'
+                fi
+                echo ''
+        done
+        cd ..
 done
-
+echo ''
+echo '[***] All emails reviewed, nothing left to process.'
+echo ''
